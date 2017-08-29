189e1586eaca19b2adffa9170c5ae2285442e047
ZhaoFeiK <zhaofei199210@gmail.com>
2017-08-29 10:09:10 +0800

    Initial Commit

---
+++ linphone-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  linphone-OC
+//
+//  Created by zhaofei on 2017/8/29.
+//  Copyright © 2017年 zhaofei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ linphone-OC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  linphone-OC
+//
+//  Created by zhaofei on 2017/8/29.
+//  Copyright © 2017年 zhaofei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
